c446c4b8d43a430895fc74372a38d8a963c48cc2
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-02-21 17:16:55 -0500

    updated App.tsx file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
+import { useAuth } from "./context/useAuth";
+import LoginForm from "./components/LoginForm";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+
+const UserInfo = () => {
+  const { user, logout } = useAuth();
+  return user ? (
+    <div>
+      <p>Welcome, {user}!</p>
+      <button onClick={logout}>Logout</button>
+    </div>
+  ) : (
+    <LoginForm />
   );
-}
+};
+
+const App = () => (
+  <AuthProvider>
+  <Router>
+    <Navbar />
+    <UserInfo />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </Router>
+</AuthProvider>
+);
 
 export default App;
